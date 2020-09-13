7a77896e016722353e49784091459c6176313739
RohaanAnsari <64054030+RohaanAnsari@users.noreply.github.com>
2020-09-13 22:35:00 +0530

    Add files via upload

---
+++ precache-manifest.cf7063da7fc79f78372f71e33c9701fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aced5bec389ca8f866ac3000ce4fe076",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52cc3ee420aec9a865e8",
+    "url": "/static/css/main.b057af10.chunk.css"
+  },
+  {
+    "revision": "71aef199f75b4662ed13",
+    "url": "/static/js/2.ea97c97c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.ea97c97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52cc3ee420aec9a865e8",
+    "url": "/static/js/main.f7f667e2.chunk.js"
+  },
+  {
+    "revision": "439ae095c7fe9cd37d99",
+    "url": "/static/js/runtime-main.0ea695f4.js"
+  }
+]);
